946e42c2d3ec1cf5dca42eb4b59aa0b6b5dfa7fa
sharnajh <sharnajhossain@gmail.com>
2018-08-02 16:15:36 -0400

    woo

--- server.rb
+++ server.rb
@@ -20,6 +20,18 @@ post '/searchapi' do
     elsif params[:yer] == "starship"
         @id = params[:search_query].to_i
         @starship_by_id = Swapir.find_starship(@id)
+        @starship_name = "Name: " + @starship_by_id["name"]
+        @starship_model = "ID: " + @starship_by_id["model"]
+        @starship_manufacturer = "Manufacturer: " + @starship_by_id["manufacturer"]
+        @starship_cost_in_credits = "Cost in credits: " + @starship_by_id["cost_in_credits"]
+        @starship_length = "Length: " + @starship_by_id["length"]
+        @starship_max_atmosphering_speed = "Max atmosphering speed: " + @starship_by_id["max_atmosphering_speed"]
+        @starship_crew = "Crew: " + @starship_by_id["crew"]
+        @starship_cargo_capacity = "Cargo capacity: " + @starship_by_id["cargo_capacity"]
+        @starship_consumables ="Consumables: " + @starship_by_id["consumables"]
+        @starship_hyperdrive_rating = "Hyperdrive rating: " + @starship_by_id["hyperdrive_rating"]
+        @starship_MGLT = "MGLT: " + @starship_by_id["MGLT"]
+        @starship_starship_class = "Starship class: " + @starship_by_id["starship_class"]
     elsif params[:yer] == "all-people"
         @all_people = Swapir.get_all_people()
     elsif params[:yer] == "all-films"
